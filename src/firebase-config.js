deaa0529ce664f7727d0500b1e38b7aa673bd8bb
Anju-Kheterpal <anjukheterpal1989@gmail.com>
2023-01-07 19:44:40 +0530

    Firebase keys changed

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -1,14 +1,33 @@
-import { initializeApp } from "firebase/app";
+// import { initializeApp } from "firebase/app";
 
+// const firebaseConfig = {
+//   apiKey: "AIzaSyBau01L2OWODZLCmFbt0eb_FZacanpjMag",
+//   authDomain: "dream-girls.firebaseapp.com",
+//   databaseURL: "https://dream-girls-default-rtdb.firebaseio.com",
+//   projectId: "dream-girls",
+//   storageBucket: "dream-girls.appspot.com",
+//   messagingSenderId: "299437353382",
+//   appId: "1:299437353382:web:492ef2e6b0edf4b5dac767",
+//   measurementId: "G-0RTR24PWVC",
+// };
+
+// export const app = initializeApp(firebaseConfig);
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBau01L2OWODZLCmFbt0eb_FZacanpjMag",
-  authDomain: "dream-girls.firebaseapp.com",
-  databaseURL: "https://dream-girls-default-rtdb.firebaseio.com",
-  projectId: "dream-girls",
-  storageBucket: "dream-girls.appspot.com",
-  messagingSenderId: "299437353382",
-  appId: "1:299437353382:web:492ef2e6b0edf4b5dac767",
-  measurementId: "G-0RTR24PWVC",
+  apiKey: "AIzaSyBZWfUaqdtAgItG4yMtgmXS3rrl_n1SIlc",
+  authDomain: "login-page-a323e.firebaseapp.com",
+  databaseURL: "https://login-page-a323e-default-rtdb.firebaseio.com",
+  projectId: "login-page-a323e",
+  storageBucket: "login-page-a323e.appspot.com",
+  messagingSenderId: "425565271310",
+  appId: "1:425565271310:web:c217da2f70ca2c2b48935f"
 };
 
-export const app = initializeApp(firebaseConfig);
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
